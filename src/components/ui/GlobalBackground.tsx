72d5c433a15e1efca624ec2138627ded9c4df4ef
MustangTris <tristanfoulds@gmail.com>
2025-12-07 14:48:45 -0800

    fixed blob animation issue

--- src/components/ui/GlobalBackground.tsx
+++ src/components/ui/GlobalBackground.tsx
@@ -12,27 +12,27 @@ export const GlobalBackground = () => {
 
             {/* Top Right - Vibrant Blue */}
             <div
-                className="absolute top-[-10%] right-[-5%] w-[70vw] h-[70vw] rounded-full bg-[#1e9cd7] opacity-20 blur-[120px] animate-blob will-change-transform"
+                className="absolute top-[-10%] right-[-5%] w-[70vw] h-[70vw] rounded-full bg-[#1e9cd7] opacity-20 blur-[120px]"
             />
 
             {/* Top Left - Pale Blue/White-ish mix for light */}
             <div
-                className="absolute top-[-5%] left-[-10%] w-[60vw] h-[60vw] rounded-full bg-[#d2ebff] opacity-40 blur-[100px] animate-blob animation-delay-2000 will-change-transform"
+                className="absolute top-[-5%] left-[-10%] w-[60vw] h-[60vw] rounded-full bg-[#d2ebff] opacity-40 blur-[100px]"
             />
 
             {/* Center Left - Deep Blue Accent */}
             <div
-                className="absolute top-[40%] left-[-15%] w-[50vw] h-[50vw] rounded-full bg-[#005d9a] opacity-10 blur-[130px] animate-blob animation-delay-4000 will-change-transform"
+                className="absolute top-[40%] left-[-15%] w-[50vw] h-[50vw] rounded-full bg-[#005d9a] opacity-10 blur-[130px]"
             />
 
             {/* Bottom Right - Vibrant Blue */}
             <div
-                className="absolute bottom-[-10%] right-[-10%] w-[60vw] h-[60vw] rounded-full bg-[#1e9cd7] opacity-15 blur-[120px] animate-blob will-change-transform"
+                className="absolute bottom-[-10%] right-[-10%] w-[60vw] h-[60vw] rounded-full bg-[#1e9cd7] opacity-15 blur-[120px]"
             />
 
             {/* Bottom Center - Pale Blue */}
             <div
-                className="absolute bottom-[-20%] left-[20%] w-[50vw] h-[50vw] rounded-full bg-[#d2ebff] opacity-30 blur-[100px] animate-blob animation-delay-2000 will-change-transform"
+                className="absolute bottom-[-20%] left-[20%] w-[50vw] h-[50vw] rounded-full bg-[#d2ebff] opacity-30 blur-[100px]"
             />
 
             {/* 3. Noise Layer - Texture (Optimized: Static Image instead of expensive SVG filter) */}
